480dae480a529a3aae9db8b46b326d9ac1d4507b
Yash Rastogi <smediayash@gmail.com>
2025-12-31 23:40:32 +0530

    Cloudinary Setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '_' + uniqueSuffix)
+    }
+})
+
+export const upload  = multer ({storage})
