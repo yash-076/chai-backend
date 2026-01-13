08c582817db74ea48334e5978e13c3baa17fca06
Yash Rastogi <smediayash@gmail.com>
2026-01-14 01:24:33 +0530

    Refactor user routes: update current user and account endpoints; add user channel profile and watch history routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,7 +8,9 @@ import {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    changeCurrentPassword
+    changeCurrentPassword,
+    getUserChannelProfile,
+    getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -34,14 +36,15 @@ router.route("/login").post(loginUser)
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
-// Current user and account
-router.route("/me").get(verifyJWT, getCurrentUser)
-router.route("/account").patch(verifyJWT, updateAccountDetails)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 // Media updates
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
